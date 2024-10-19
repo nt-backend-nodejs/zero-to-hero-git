75311c9b5ba0669113944940d737d9396a838007
root <rayadunia2021@gmail.com>
2024-10-19 12:53:51 +0500

    Roters

--- src/routes/log.routes.js
+++ src/routes/log.routes.js
@@ -1,1 +1,21 @@
-console.log("Salomatmi");
+
+import express from "express";
+
+productsRouter.get("/",(req,res,next)=>{
+    console.log("Ali");
+    
+
+}),
+
+//GET BY ID
+productsRouter.put("/:id"(req,res,next)=>{
+    console.log("Vali")
+    
+}),
+
+//CREATE
+productsRouter.post("/"(req,res,next)=>{
+    console.log();
+    
+})
+
